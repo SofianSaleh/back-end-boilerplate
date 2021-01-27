5c472671bc7588d6c7344f07e3f43ad046226410
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-27 19:56:59 +0200

    :fire: done

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -3,7 +3,7 @@ import { Unauthorized } from '../error/unauthorized.error';
 import { HttpExpress } from '../security/httpExpress.security';
 import { JWT } from '../security/jwt.security';
 
-export default function (req: Request, res: Response, next: NextFunction) {
+export default function (req: Request, _res: Response, next: NextFunction) {
   const token: string = HttpExpress.retriveBearerTokenFromRequest(req);
 
   if (!JWT.isTokenValid(token, false)) throw new Unauthorized(`Unauthorized`);
