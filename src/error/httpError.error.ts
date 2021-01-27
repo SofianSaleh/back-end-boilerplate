f4abe8dd20ae8bd5bdb1d7a3fb7cb7eb8ab4a4c6
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-27 19:57:46 +0200

    :fire: done

--- src/error/httpError.error.ts
+++ src/error/httpError.error.ts
@@ -2,7 +2,7 @@ export abstract class HttpError implements Error {
   public name: string;
   public message: string;
   public stack?: string;
-  // public abstract statusCode: number;
+  public abstract statusCode: number;
 
   constructor(message: string) {
     this.message = message;
