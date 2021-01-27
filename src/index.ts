fd651abcf473e4da1fafd09bba6306632cb1ba35
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-27 19:56:06 +0200

    :fire: done

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ import { UsersController } from './controller/User.controller';
 import { errorMiddleware } from './middleware/error.middleware';
 import { AuthController } from './controller/Auth.controller';
 
-// import * as jwt from 'jsonwebtoken';
-// app.get('/', (req, res) => {
-//   const { id } = jwt.decode(
-//     `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY1NDZiMGMwLTVkNTEtNDRjMS05YWVhLWMwZTEzOTNjZWJkYSIsImVtYWlsIjoic29maWFuMTFpMUB5YWhvby5jb20iLCJpYXQiOjE2MTE1MDY5MzEsImV4cCI6MTYxMTUwNzgzMSwic3ViIjoiZjU0NmIwYzAtNWQ1MS00NGMxLTlhZWEtYzBlMTM5M2NlYmRhIiwianRpIjoiMjM1ZjFjMjItM2UzOS00Y2EzLWE5ZTUtMDY2YWEyN2I1YmRmIn0.Z2NlhwmIg_WGKW6Ta_hMrQPlWHIc6spa8nHdtVeLbB0`
-//   );
-//   res.json(id);
-// });
+import * as jwt from 'jsonwebtoken';
+app.get('/', (req, res) => {
+  const { id } = jwt.decode(
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY1NDZiMGMwLTVkNTEtNDRjMS05YWVhLWMwZTEzOTNjZWJkYSIsImVtYWlsIjoic29maWFuMTFpMUB5YWhvby5jb20iLCJpYXQiOjE2MTE1MDY5MzEsImV4cCI6MTYxMTUwNzgzMSwic3ViIjoiZjU0NmIwYzAtNWQ1MS00NGMxLTlhZWEtYzBlMTM5M2NlYmRhIiwianRpIjoiMjM1ZjFjMjItM2UzOS00Y2EzLWE5ZTUtMDY2YWEyN2I1YmRmIn0.Z2NlhwmIg_WGKW6Ta_hMrQPlWHIc6spa8nHdtVeLbB0`
+  );
+  res.json(id);
+});
 
 const app = express();
 const appBuilder = new AppBuilder(app);
